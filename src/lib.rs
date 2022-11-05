3e8874bc4d90f0b4ed87c349b47276dedb71d61b
gaoqiangz <zhanggaoqiang@jqzms.com>
2022-11-06 01:42:25 +0800

    1.实现`describe` 2.增加`case_insensitive`特性，支持不区分参数名大小写模式

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,36 @@ mod parser;
 mod ast;
 
 mod prelude {
-    #[cfg(feature = "indexmap")]
+    #[cfg(feature = "preserve_order")]
     pub use indexmap::IndexMap as HashMap;
     pub use std::borrow::Cow;
-    #[cfg(not(feature = "indexmap"))]
+    #[cfg(not(feature = "preserve_order"))]
     pub use std::collections::HashMap;
+
+    #[cfg(feature = "case_insensitive")]
+    pub type KeyType<'a> = unicase::Ascii<Cow<'a, str>>;
+    #[cfg(not(feature = "case_insensitive"))]
+    pub type KeyType<'a> = Cow<'a, str>;
+
+    pub trait IntoKey<'a> {
+        fn into_key(self) -> KeyType<'a>;
+    }
+
+    #[cfg(feature = "case_insensitive")]
+    impl<'a, T> IntoKey<'a> for T
+    where
+        T: Into<Cow<'a, str>>
+    {
+        fn into_key(self) -> KeyType<'a> { KeyType::new(self.into()) }
+    }
+
+    #[cfg(not(feature = "case_insensitive"))]
+    impl<'a, T> IntoKey<'a> for T
+    where
+        T: Into<Cow<'a, str>>
+    {
+        fn into_key(self) -> KeyType<'a> { self.into() }
+    }
 }
 
 pub use ast::*;
---
+++ src/parser/value.rs
@@ -0,0 +1,70 @@
+
+use super::*;
+
+/// 字面值解析
+pub fn literal(input: &str) -> ParseResult<Value> {
+    /// 普通字面值
+    fn normal(input: &str) -> ParseResult<&str> {
+        take_while1(|c: char| c.is_alphanumeric() || c == '.' || c == '_')(input)
+    }
+    /// 带括号的字面值
+    fn with_paren(orig_input: &str) -> ParseResult<&str> {
+        use nom::{Offset, Slice};
+        let (input, _) = terminated(alpha1, multispace0)(orig_input)?;
+        let (input, _) = delimited(tag("("), delimited(multispace0, normal, multispace0), tag(")"))(input)?;
+        //输出原始文本
+        let offset = orig_input.offset(&input);
+        Ok((input, orig_input.slice(..offset)))
+    }
+    //必须是字母开头
+    satisfy(|c| c.is_alphabetic())(input)?;
+    let parser = alt((with_paren, normal)).map(|v| Value::Literal(v.into()));
+    context("literal", parser)(input)
+}
+
+/// 字符串解析
+pub fn string(input: &str) -> ParseResult<Value> {
+    /// 不同引号字符串(`""`/`''`)转义处理
+    fn quoted(qot: char) -> impl Fn(&str) -> ParseResult<&str> {
+        move |input: &str| {
+            delimited(
+                char(qot),
+                escaped(
+                    take_till1(|c: char| c == '~' || c == qot),
+                    '~',
+                    anychar //PB字符串转义不限制字符
+                ),
+                char(qot)
+            )(input)
+        }
+    }
+    //NOTE
+    //`escaped`要求`normal`参数失败才会匹配`control_char`,所以必须使用`take_till1`组合子
+    //因此需要单独匹配空字符串: `""`/`''`
+    let double_quoted =
+        alt((quoted('"'), tag("\"\"").map(|_| ""))).map(|v| Value::DoubleQuotedString(v.into()));
+    let single_quoted =
+        alt((quoted('\''), tag("''").map(|_| ""))).map(|v| Value::SingleQuotedString(v.into()));
+    let parser = alt((double_quoted, single_quoted));
+    context("string", parser)(input)
+}
+
+/// 数值解析
+pub fn number(input: &str) -> ParseResult<Value> { context("number", double.map(Value::Number))(input) }
+
+/// 多值列表解析
+pub fn list(input: &str) -> ParseResult<Value> {
+    let parser = delimited(
+        tag("("),
+        separated_list0(tag(","), delimited(multispace0, alt((string, literal, list, fail)), multispace0))
+            .map(Value::List),
+        tag(")")
+    );
+    context("list", parser)(input)
+}
+
+/// Key-Value值列表解析
+pub fn map(input: &str) -> ParseResult<Value> {
+    let parser = value_map.map(Value::Map);
+    context("map", parser)(input)
+}
